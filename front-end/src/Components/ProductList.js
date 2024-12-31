a3c1242f908ac93c23261faf1dd4f37e5fec819e
rahuldev9 <webdeveloperaim@gmail.com>
2024-12-31 22:02:17 +0530

    Update ProductList.js

--- front-end/src/Components/ProductList.js
+++ front-end/src/Components/ProductList.js
@@ -14,7 +14,7 @@ function ProductList() {
 
   const getProducts = async () => {
     const userid = JSON.parse(localStorage.getItem("user"))._id;
-    let result = await fetch(`http://localhost:4500/products/${userid}`, {
+    let result = await fetch(`https://story-api-pgo4.onrender.com/products/${userid}`, {
       headers: {
         authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
       },
@@ -25,7 +25,7 @@ function ProductList() {
   };
 
   const deleteproduct = async (id) => {
-    let result = await fetch(`http://localhost:4500/product/${id}`, {
+    let result = await fetch(`https://story-api-pgo4.onrender.com/product/${id}`, {
       method: "DELETE",
       headers: {
         authorization: `bearer ${JSON.parse(localStorage.getItem("token"))}`,
